1f5619af72e0686d9e67ef617ae9a2fd2470e39d
samw2703 <samw2703@gmail.com>
2021-11-26 16:22:34 +0000

    add test that stub actually gets added

--- GenericRepository.Stub.Tests/StubbedGenericRepositoryBuilderTests.cs
+++ GenericRepository.Stub.Tests/StubbedGenericRepositoryBuilderTests.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Linq;
+using Callinho;
+using GenericRepository.Abstractions;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 
@@ -7,24 +10,27 @@ namespace GenericRepository.Stub.Tests
 	public class StubbedGenericRepositoryBuilderTests
 	{
 		[Test]
-		public void Add_RepoThatHasNotYetBeenAdded_DoesNotThrow()
+		public void Add_RepoThatHasNotYetBeenAdded_AddsRepo()
 		{
-			Assert.DoesNotThrow(
-				() => CreateBuilder().Add<RepoItem, int>(x => x.Id));
+			var serviceCollection = new ServiceCollection();
+			new StubbedGenericRepositoryBuilder(serviceCollection)
+				.Add<RepoItem, int>(x => x.Id);
+
+			serviceCollection
+				.Any(x => x.ServiceType == typeof(IGenericRepository<RepoItem, int>) && x.ImplementationInstance?.GetType() == typeof(GenericStubbedRepository<RepoItem,int>))
+				.Use(Assert.True);
 		}
 
 		[Test]
 		public void Add_RepoThatHasAlreadyBeenAdded_Throws()
 		{
 			var ex = Assert.Throws<ArgumentException>(
-				() => CreateBuilder()
+				() => new ServiceCollection()
+					.Call(sc => new StubbedGenericRepositoryBuilder(sc))
 					.Add<RepoItem, int>(x => x.Id)
 					.Add<RepoItem, int>(x => x.Id));
 			Assert.AreEqual("A repository for GenericRepository.Stub.Tests.StubbedGenericRepositoryBuilderTests+RepoItem with key System.Int32", ex.Message);
 		}
-
-		private StubbedGenericRepositoryBuilder CreateBuilder()
-			=> new(new ServiceCollection());
 
 		private class RepoItem
 		{
